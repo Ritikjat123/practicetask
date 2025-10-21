c31ad24d414a6c0b88fc514e3ce7d42a0922f264
Ritik Jat <ritikdudee00@gmail.com>
2025-10-21 22:50:09 +0530

    Add files via upload

---
+++ pA2/pA2.js
@@ -0,0 +1,6 @@
+let errorParagraph=document.getElementById("error")
+console.log(errorParagraph)
+function purchase(){
+   console.log("button clicked")
+   errorParagraph.textContent="something went wronge ,please try again!"
+}
