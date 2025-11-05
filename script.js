ae8caca9d2f426737f8d5e09246e5b52564d0cc5
Ritik Jat <ritikdudee00@gmail.com>
2025-11-05 23:50:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+
+// function apiCall(url){
+  
+//        fetch(url)
+//        .then((res)=>res.json())
+//   .then(
+//     (data)=>console.log(data,"data is fetched")
+    
+//   ).catch((err)=>reject(err,"error"))
+// }
+// apiCall("https://fakestoreapi.com/products");
+
+// let arr=[1,4,7,5,3,2];
+
+// for(let i =0;i<arr.length;i++){
+// for(let j=0;j<arr.length;j++){
+//  if(arr[i]>arr[j]){
+//     let temp=arr[j]
+//     arr[j]=arr[i]
+//     arr[i]=temp;
+//  }
+// }
+
+// }console.log(arr)
+
+// let arr=[1,4,7,5,3,2];
+
+// for(let i =0;i<arr.length;i++){
+// for(let j=0;j<arr.length;j++){
+//  if(arr[i]<arr[j]){
+//     let temp=arr[j]
+//     arr[j]=arr[i]
+//     arr[i]=temp;
+//  }
+// }
+
+/ / }console.log(arr)
+
+// let arr=[1,4,7,5,3,2];
+
+// for(let i =0;i<arr.length;i++){
+// let temp = arr[i]*2;
+// arr[i]=temp
+
+// }console.log(arr)
+
+// let arr=[1,4,7,5,3,99,2];
+// let arr2=[];
+// for(let i =0;i<arr.length;i++){
+// for(let j=0;j<arr.length;j++){
+//  if(arr[i]<arr[j]){
+//     let temp=arr[j]
+//     arr[j]=arr[i]
+//     arr[i]=temp;
+//  }
+// }
+// }console.log(arr)
+// for(let i=0;i<arr.length;i++){
+//   console.log(arr[i]*2)
+// }
+
