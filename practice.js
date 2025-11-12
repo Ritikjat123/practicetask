a75eacb3dfc3287dd3692d6262f1f883ba8d3123
Ritik Jat <ritikdudee00@gmail.com>
2025-11-12 23:27:02 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,47 @@
+// let arr=["aabc","bcda","dabc","abcd","abca"];
+// arr.sort();
+// console.log(arr);
+
+// //without sort method
+
+// for(let i=0;i<arr.length;i++){
+//     let temp=[];
+//     for(let j=i+1;j<arr.length;j++){
+//         if(arr[i]<arr[j]){
+//              temp=arr[i];
+//              arr[i]=arr[j];
+//              arr[j]=temp;
+//         }
+//     }
+// }
+// console.log(arr)
+
+// let arr=["aabc","bcda","dabc","abcd","abca"];
+
+// let str=arr[0].toLocaleUpperCase();
+// console.log(str);
+
+// let str="hello world";
+
+// let res='';
+
+// for(let i=str.length-1;i>=0;i--){
+//     res+=str[i];
+// }console.log(res)
+
+// let str="hello world";
+
+// let str2=str.slice(0,6)+str.charAt(6).toUpperCase()+str.slice(7,str.length);
+// console.log(str2)
+
+// let str="hello world";
+// let str2='';
+// for(let i=0;i<str.length;i++){
+//     str2+=str.charAt(i).toUpperCase();
+// }
+// console.log(str2)
+
+// let str="hello world";
+// let str2=str.toUpperCase();
+// console.log(str2)
+
